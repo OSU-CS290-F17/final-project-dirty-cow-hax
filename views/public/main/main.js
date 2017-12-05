a79c157ebada8d60009255c4b08457ecd7b8df88
Shane Clancy <clancys@oregonstate.edu>
2017-12-04 19:27:10 -0800

    Ability to add entry to database and reload page

--- views/public/main/main.js
+++ views/public/main/main.js
@@ -4,5 +4,34 @@ const backdrop = document.getElementById('modal-background');
 
 entryButton.addEventListener('click', (event) => {
     entryModal.classList.remove('hidden');
-    backdrop.classLost.remove('hidden');
+    backdrop.classList.remove('hidden');
+});
+
+backdrop.addEventListener('click', (event) =>{
+    entryModal.classList.add('hidden');
+    backdrop.classList.add('hidden');
+});
+
+const entryCreate = document.getElementById('create-button');
+
+entryCreate.addEventListener('click', (event) =>{
+    let id = window.location.pathname;
+    id = id.slice(8);
+    
+    const entryObj = {
+        _id : id,
+    }
+    
+    const postData = JSON.stringify(entryObj);
+    const postURL = `/people/${id}/new`;
+    const request = new XMLHttpRequest();
+    request.open('POST', postURL);
+
+    request.addEventListener('load', (event) => {
+        window.location = `/people/${id}`;
+    });
+
+    request.send(postData);
+    entryModal.classList.add('hidden');
+    backdrop.classList.add('hidden');
 });
